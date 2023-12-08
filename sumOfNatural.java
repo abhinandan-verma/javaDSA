a1e7000934fe1b408a78ca01a7f55bccbc78b0d6
abhinandan-verma <abhinandanverma551@gmail.com>
2023-12-08 18:19:36 +0530

    Initial Commit

---
+++ sumOfNatural.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sumOfNatural {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Natural no. for getting Sum");
+
+        int n = sc.nextInt();
+        
+        calculateSum(1, n, 0);
+        sc.close();
+    }
+
+   public static void calculateSum(int i,int n,int sum){
+    if (i == n) {
+        sum += n;
+        System.out.println("the sum is = "+sum);
+        return;
+    }
+    sum += i;
+    calculateSum(i+1, n, sum);
+   }
+}
