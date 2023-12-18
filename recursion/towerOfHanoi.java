f8c1131c14c3d419e4ddceed1fa0cd42900b94d6
abhinandan-verma <abhinandanverma551@gmail.com>
2023-12-18 22:03:35 +0530

    test IntelliJ

---
+++ recursion/towerOfHanoi.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class towerOfHanoi {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENter the n ");
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println("Hello Abhinandan");
+    }
+}
