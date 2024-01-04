82da0b007a217615185ad264bcbbab5b820b7d97
abhinandan-verma <abhinandanverma551@gmail.com>
2024-01-04 15:03:06 +0530

    uniqueSubsequeces

--- recursion/allSubsequences.java
+++ recursion/allSubsequences.java
@@ -1,25 +1,31 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class allSubsequences {
 
-    public static void subsequences(String str, int idx, String newString){
+    public static void subsequences(String str, int idx, String newString, HashSet<String> set){
         if (idx == str.length()) {
+            if (set.contains(newString)) {
+                return ; 
+            }
             System.out.println(newString);
+            set.add(newString);
             return;
         }
         char currChar = str.charAt(idx);
         // to be
-            subsequences(str, idx+1, newString+currChar);
+            subsequences(str, idx+1, newString+currChar,set);
         // not to be
-            subsequences(str, idx+1, newString);
+            subsequences(str, idx+1, newString, set);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the String to get Subsequences : ");
+        System.out.print("Enter the String to get Subsequences : ");
         String str = sc.nextLine();
+        HashSet<String> set = new HashSet<>();
 
-        subsequences(str, 0, "");
+        subsequences(str, 0, "",set);
         sc.close();
         
     }
